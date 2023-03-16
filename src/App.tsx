2d9f3b31e16c531b374c536a29e52e27011c44eb
Aleksvtop <vazilino@gmail.com>
2023-03-16 16:28:54 +0300

    Use hooks useState & useEffect for rendering tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 
 import './App.css'
 
 function App() {
-    let tags = [{id:1, title: "HTML"}, {id:2, title: "CSS"}, {id:3, title: "JS"}, {id:4, title: "React"}];
-  return (
-    <div>
-      <ul>
-          {
-              tags.map(t =>
-                  <div id={t.id.toString()}>{t.title}</div>)
-          }
+    const [tags, setTags] = useState<any[]>([])
 
-      </ul>
-    </div>
-  )
+    //2. register effect
+    useEffect(() => {
+        //4.run effect
+        setTimeout(() => {
+            //5. change observable state
+            setTags([{id: 1, title: "HTML"},
+                {id: 2, title: "CSS"},
+                {id: 3, title: "JS"},
+                {id: 4, title: "React"}]
+            )
+        }, 2000)
+    }, [])
+    //3.empty rendering
+    //7.tags rendering
+    return (
+        <div>
+            <ul>
+                {
+                    tags.map((t: any) => {
+                            return <div key={t.id.toString()}>{t.title}</div>
+                        }
+                    )
+                }
+
+            </ul>
+        </div>
+    )
 }
 
 export default App
